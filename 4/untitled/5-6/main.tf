3b831820e2743366e480b8d804eaf7f3f0a966ee
yongsang123 <hichoys123@gmail.com>
2022-08-24 17:13:27 +0900

    AWS_LoadBalancer_Step6 (4-3 + lb)

--- 4/untitled/5-6/main.tf
+++ 4/untitled/5-6/main.tf
@@ -1,5 +1,5 @@
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = "aws_launch_configuration.example.com"
+  launch_configuration = aws_launch_configuration.example.com
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
